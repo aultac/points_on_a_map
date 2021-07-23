3205d98c7938211fddf7b2ae5cc0b3dc3be20c8e
Aaron Ault <aultac@gmail.com>
2021-07-23 18:06:05 -0400

    Added geohashes

--- src/overmind/actions.js
+++ src/overmind/actions.js
@@ -1,5 +1,7 @@
 import { connect } from '@oada/client';
 import idclient from '@oada/oada-id-client';
+import Promise from 'bluebird';
+import _ from 'lodash';
 
 import metadata from '../login/signed_software_statement';
 
@@ -14,13 +16,40 @@ export function incrementCount({state}) {
 
 export async function loadOADAData({state}) {
   try {
+    state.points = {};
+
     const oada = await connect({domain: state.domain, token: state.token});
-    state.points = await oada.get({path: '/bookmarks/points_on_a_map'}).then(r=>r.data);
+
+    // Pull the geohash index:
+    const geohashes = await oada
+      .get({path: '/bookmarks/points_on_a_map/geohash-index'})
+      .then(r=>r.data)
+      .then(g => Object.keys(g));
+
+    // Get each of the geohash buckets:
+    let mapcentered = false;
+    await Promise.map(geohashes, async geohash => {
+      const newpoints = await oada.get({ path: `/bookmarks/points_on_a_map/geohash-index/${geohash}` }).then(r=>r.data);
+      const keys = Object.keys(newpoints);
+      for( let i=0; i<keys.length; i++) {
+        const key = keys[i];
+        if (!mapcentered) {
+          const lat = +(newpoints[key].lat);
+          const lon = +(newpoints[key].lon);
+          if (lat && lon) {
+            state.center = [ lat, lon ];
+            mapcentered = true;
+          }
+        }
+        state.points[key] = newpoints[key]
+      }
+    }, { concurrency: 5 });
+
   
-    console.log('Setting center to ', state.pointsArray[0]);
-    state.center = state.pointsArray[0];
+    //console.log('Setting center to ', state.pointsArray[0]);
+    //state.center = state.pointsArray[0];
   } catch (e) {
-    alert(`Failed to connect to OADA at domain ${state.domain} with token ${state.token}`);
+    alert(`Failed to connect to OADA at domain ${state.domain} with token ${state.token}.  Error was: ${e.toString()}`);
   }
 }
 
